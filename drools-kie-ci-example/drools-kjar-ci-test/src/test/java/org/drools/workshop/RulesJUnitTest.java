9ce2c9a45dab1edba45b4a4a9456b7e01a042f7f
salaboy <salaboy@gmail.com>
2015-10-21 23:46:32 +0100

    - kie-ci example

---
+++ drools-kie-ci-example/drools-kjar-ci-test/src/test/java/org/drools/workshop/RulesJUnitTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.drools.workshop;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.api.cdi.KReleaseId;
+import org.kie.api.cdi.KSession;
+import org.kie.api.runtime.KieSession;
+
+/**
+ *
+ * @author salaboy
+ */
+@RunWith(Arquillian.class)
+public class RulesJUnitTest {
+
+    public RulesJUnitTest() {
+    }
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+//        System.out.println(jar.toString(true));
+        return jar;
+    }
+    
+    @Inject
+    @KSession
+    @KReleaseId(groupId = "org.drools.workshop",artifactId = "my-first-drools-kjar", version = "1.0-SNAPSHOT")
+    private KieSession kSession;
+   
+     @Test
+     public void hello() {
+        Assert.assertNotNull(kSession);
+        kSession.insert("Hi There From Test!");
+        Assert.assertEquals(1, kSession.fireAllRules());
+     }
+}
